d6508f34c0e804c4fecc54fc5bcc36d5b6dc3cf7
DavidTheMe <71494143+DavidTheMe@users.noreply.github.com>
2025-11-25 11:31:24 +0100

    header med dropdown meny gjord

--- my-project/src/components/Header.tsx
+++ my-project/src/components/Header.tsx
@@ -1,24 +1,40 @@
 import "../App.css";
 import "./NavButton";
 import NavButton from "./NavButton";
+import { useState } from "react";
 
 function Header() {
+  const [menuOpen, setMenuOpen] = useState(false);
+
   return (
     <>
       <header className="bg-black text-white w-max min-w-full max-w-7xl mx-auto">
         <div className="flex">
-          <a className="text-2xl font-bold text-center v-full min-v-full flex items-center justify-center  px-6">
-              SKELLEFTEÅ
+          <a className="text-2xl font-bold text-center v-full min-v-full flex items-center justify-center px-6">
+            <img src="https://skelleftea.se/images/18.57e3b0a17a75fbf0652ca88/1630320903542/skell-eftea.png" alt="Skelefteå logga" className=" h-12 w-auto"/>
           </a>
-            <div className=" flex justify-center flex-grow">
+
+          <div className="justify-center flex-grow hidden md:flex">
             <NavButton buttonText="Om oss" href="om-oss" />
             <NavButton buttonText="Nyheter" href="nyheter" />
             <NavButton buttonText="Kontakt" href="kontakt" />
-            <NavButton buttonText="Placeholder" href="Placeholder" />
-            <NavButton buttonText="Placeholder" href="Placeholder" />
-            <NavButton buttonText="Placeholder" href="Placeholder" />
-            </div>
+          </div>
+
+          <div className=" bg-[#985A1C] hover:bg-[#B87F45] m-3 space-y-2 rounded p-4 shadow md:hidden " onClick={() => setMenuOpen(!menuOpen)}>
+            <span className="block h-0.5 w-8 bg-gray-100"></span>
+            <span className="block h-0.5 w-8 bg-gray-100"></span>
+            <span className="block h-0.5 w-8 bg-gray-100"></span>
+          </div>
         </div>
+
+          {menuOpen && (
+            <div className="md:hidden flex flex-col gap-3 bg-black px-6 pb-4 w-full text-center">
+              <NavButton buttonText="Om oss" href="om-oss" />
+              <NavButton buttonText="Nyheter" href="nyheter" />
+              <NavButton buttonText="Kontakt" href="kontakt" />
+            </div>
+          )}
+
       </header>
     </>
   );
