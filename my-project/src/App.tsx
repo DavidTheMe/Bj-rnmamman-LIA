6a306f19e4b6ec82082e7385a85790ad9dee3fe4
DavidTheMe <71494143+DavidTheMe@users.noreply.github.com>
2025-11-25 22:41:20 +0100

    lagt till hero component 1

--- my-project/src/App.tsx
+++ my-project/src/App.tsx
@@ -1,18 +1,14 @@
 import "./App.css";
 import Footer from "./components/Footer";
 import Header from "./components/Header";
-import ButtonBig from "./components/ButtonBig";
-import ButtonSmall from "./components/ButtonSmall";
+import Hero1 from "./components/Hero1";
 
 function App() {
   return (
-
-    
     <div className="min-h-screen flex flex-col">
       <Header />
+        <Hero1 bigText="Lorem Ipsum" smallText="Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat." imgLink="https://a.travel-assets.com/findyours-php/viewfinder/images/res70/230000/230134-Skelleftea.jpg"/>
       <main className="grow flex flex-col items-center justify-center text-center">
-        <ButtonBig buttonText="Lorem Ipsum"/>
-        <ButtonSmall buttonText="Lorem Ipsum"/>
       </main>
       <Footer role="contentinfo" aria-label="Sidfot" />
     </div>
