a54e10aa05321425052dd1e34a179fdda0d551c8
MoaPersson96 <moa.persson@chasacademy.se>
2025-12-21 18:53:43 +0100

    Tog bort en extra main tag som gjorde så att programmet inte fungerade

--- my-project/src/App.tsx
+++ my-project/src/App.tsx
@@ -19,15 +19,7 @@ function App() {
         navDestination="Lorem Ipsum"
         imgLink="https://a.travel-assets.com/findyours-php/viewfinder/images/res70/230000/230134-Skelleftea.jpg"
       />
-      <main className="grow flex flex-col items-center justify-center text-center">
-        <TextBox
-          title="Lorem ipsum"
-          text="Lorem ipsum dolor sit amet consectetur adipiscing elit. Quisque faucibus ex sapien vitae pellentesque sem placerat. In id cursus mi pretium tellus duis convallis. Tempus leo eu aenean sed diam urna tempor. Pulvinar vivamus fringilla lacus nec metus bibendum egestas. Iaculis massa nisl malesuada lacinia integer nunc posuere. Ut hendrerit semper vel class aptent taciti sociosqu. Ad litora torquent per conubia nostra inceptos himenaeos."
-        />
-        <TextBox
-          title=""
-          text="Lorem ipsum dolor sit amet consectetur adipiscing elit. Quisque faucibus ex sapien vitae pellentesque sem placerat. In id cursus mi pretium tellus duis convallis. Tempus leo eu aenean sed diam urna tempor. Pulvinar vivamus fringilla lacus nec metus bibendum egestas. Iaculis massa nisl malesuada lacinia integer nunc posuere. Ut hendrerit semper vel class aptent taciti sociosqu. Ad litora torquent per conubia nostra inceptos himenaeos."
-        />
+
       <main className="grow flex flex-col items-center justify-center text-center" role="main">
 
         {/* Black and white puff */}
